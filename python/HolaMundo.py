f9693e03bbd15a4990c6976071837953209da647
IsaiasCordoba <101846332+IsaiasCordoba@users.noreply.github.com>
2022-03-17 19:49:43 -0600

    Actualización

---
+++ python/HolaMundo.py
@@ -0,0 +1,7 @@
+# COMENTARIO: Ejemplo 1 Saludo
+# Los comentarios sirven para documentar y aclarar cada parte
+# del programa en python
+
+print("Hola Mundo")
+print('ISAIAS GODINEZ CORDOBA');
+print(10+20)

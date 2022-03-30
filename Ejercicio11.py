e288279d54d7991cabdb0b0b95098b81d6863ffc
Isaias <101846332+IsaiasCordoba@users.noreply.github.com>
2022-03-30 17:52:58 -0600

    actualización

---
+++ Ejercicio11.py
@@ -0,0 +1,15 @@
+# Hacer una función para que imprima un arreglo o lista de 10 elementos numéricos
+
+arreglo_numeros = [7, 8,  5,  3,  2,  7,  9,  12, 15, 13]
+# Índice (Índex)   0  1   2   3   4   5   6   7   8   9 
+
+
+  
+# Operaciones con arereglos
+
+arreglo_numeros.sort()
+arreglo_numeros.reverse()
+
+
+# SALIDA
+print(arreglo_numeros)
